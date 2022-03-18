d6468bc81e84bffabcac38696cc6acee18e9bad6
wmonteiro92 <32559170+wmonteiro92@users.noreply.github.com>
2022-03-17 22:41:49 -0300

    Removendo imports não-utilizados.

--- .ipynb_checkpoints/predictions-checkpoint.py
+++ .ipynb_checkpoints/predictions-checkpoint.py
@@ -10,9 +10,6 @@ import pandas as pd
 import numpy as np
 import pickle
 
-import plotly.express as px
-import plotly.graph_objects as go
-
 import tensorflow as tf
 import tensorflow_recommenders as tfrs
 
